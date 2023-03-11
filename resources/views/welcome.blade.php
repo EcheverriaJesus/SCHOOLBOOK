30fb3aa5edb88c2ec45020569b28d4cb7c368cf8
noe <jesus.echeverria017@gmail.com>
2023-03-10 20:04:16 -0600

    Login terminado, se desecho la vista welcome de la carpeta teams y en su lugar se ocupa login de la carpeta auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,15 +10,15 @@
     <title>Login3</title>
 </head>
 
-<body class="p-20">
+<body class="p-20 bg-gray-900 h-screen">
 
-    <header class="">
+    {{-- <header class="">
                 @if (Route::has('login'))
                     <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
                         @auth
                             <a href="{{ url('/dashboard') }}"
                                 class="font-semibold text-gray-600 hover:text-blue-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Iniciar
-                                Sesion</a>{{-- no es necesario logearse si ya esta en una sesion activa --}}
+                                Sesion</a>
     @else
                             <a href="{{ route('login') }}"
                                 class="font-semibold text-gray-600 hover:text-gray-900 focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Iniciar
@@ -30,11 +30,10 @@
                         @endauth
                     </div>
                 @endif
-           </header>
+           </header> --}}
 
-    <div class="h-auto md:flex">
-        <section
-            class="rounded-2xl relative overflow-hidden md:flex w-1/2 bg-gradient-to-tr from-blue-800 to-purple-700 i justify-around items-center hidden">
+    <div class="h-full md:flex">
+        <section class="rounded-2xl relative overflow-hidden md:flex w-1/2 bg-gradient-to-tr from-blue-800 to-purple-700 i justify-around items-center hidden">
             <div>
                 <img class="w-64º h-40 pt-10" src="../images/logo_schoolbook_blanco.png" alt="">
                 {{-- <h1 class="text-white font-bold text-4xl font-sans">SchoolBook</h1> --}}
@@ -49,32 +48,85 @@
         </section>
 
         <section class="flex md:w-1/2 justify-center py-10 items-center bg-white">
-            <form class="bg-white">
-                <h1 class="text-gray-800 font-bold text-2xl mb-1">Bienvenido a SchoolBok!</h1>
-                <p class="text-sm font-normal text-gray-600 mb-7">Sistema de Control Escolar "Vicente Guerrero Saldaña"</p>
-                <div class="flex items-center border-2 py-2 px-3 rounded-2xl mb-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" fill="none"
-                        viewBox="0 0 24 24" stroke="currentColor">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
-                    </svg>
-                    <input class="pl-2 outline-none border-none" type="text" name="" id=""
-                        placeholder="Matricula" />
+            @if (session('status'))
+            <div class="mb-4 font-medium text-sm text-green-600">
+                {{ session('status') }}
+            </div>
+        @endif
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+    
+                <div>
+                    <x-label for="email" value="{{ __('Email') }}" />
+                    <x-input id="email" class="pl-4 block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" placeholder="Matricula"/>
                 </div>
-                <div class="flex items-center border-2 py-2 px-3 rounded-2xl">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 text-gray-400" viewBox="0 0 20 20"
-                        fill="currentColor">
-                        <path fill-rule="evenodd"
-                            d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z"
-                            clip-rule="evenodd" />
-                    </svg>
-                    <input class="pl-2 outline-none border-none" type="text" name="" id=""
-                        placeholder="Contraseña" />
+    
+                <div class="mt-4">
+                    <x-label for="password" value="{{ __('Password') }}" />
+                    <x-input id="password" class=" pl-4 block mt-1 w-full" type="password" name="password" required autocomplete="current-password" placeholder="Contraseña"/>
                 </div>
-                <button type="submit"
-                    class="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2">Iniciar Sesion</button>
-                <span class="text-sm ml-2 hover:text-blue-500 cursor-pointer">¿Olvido su contraseña?</span>
+    
+                <div class="block mt-4">
+                    <label for="remember_me" class="flex items-center">
+                        <x-checkbox id="remember_me" name="remember" />
+                        <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                    </label>
+                </div>
+    
+                <div class="flex items-center justify-end mt-4">
+                    @if (Route::has('password.request'))
+                        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                            {{ __('Forgot your password?') }}
+                        </a>
+                    @endif
+    
+                    <x-button class="ml-4">
+                        {{ __('Log in') }}
+                    </x-button>
+                </div>
             </form>
+                {{-- <x-validation-errors class="mb-4" />
+        
+                @if (session('status'))
+                    <div class="mb-4 font-medium text-sm text-green-600">
+                        {{ session('status') }}
+                    </div>
+                @endif
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <h1 class="text-gray-800 font-bold text-2xl mb-1">Bienvenido a SchoolBok!</h1>
+                    <p class="text-sm font-normal text-gray-600 mb-7">Sistema de Control Escolar "Vicente Guerrero Saldaña"</p>
+    
+                    <div>
+                        <x-label for="email" value="{{ __('Email') }}" />
+                        <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
+                    </div>
+        
+                    <div class="mt-4">
+                        <x-label for="password" value="{{ __('Password') }}" />
+                        <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                    </div>
+        
+                    <div class="block mt-4">
+                        <label for="remember_me" class="flex items-center">
+                            <x-checkbox id="remember_me" name="remember" />
+                            <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+                        </label>
+                    </div>
+        
+                    <div class="flex items-center justify-end mt-4">
+                        @if (Route::has('password.request'))
+                            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                                {{ __('Forgot your password?') }}
+                            </a>
+                        @endif
+        
+                        <x-button class="ml-4">
+                            {{ __('Log in') }}
+                        </x-button>
+                    </div>
+                </form>
+          --}}
         </section>
     </div>
 
--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/* Route::get('/', function () {
     return view('welcome');
+}); */
+Route::get('/', function () {
+    return view('auth.login');
 });
 
 Route::middleware([
